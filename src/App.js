45ecacdee5450644ee264869ee55435b9f7fd6f1
Amine MEBARKI <medaminemebarki@gmail.com>
2022-06-20 17:02:39 +0200

    mise a jour de la page avec css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 
 import Banner from './components/Banner'
+import logo from './assets/logo.png'
 import Cart from './components/Cart'
-import ShoppingList from './components/ShoppingList';
 import Footer from './components/Footer'
+import ShoppingList from './components/ShoppingList'
+import './styles/Layout.css'
 
 function App() {
- return (
-  <div>
-   <Banner />
-   <Cart />
-   <ShoppingList />
-   <Footer />
-  </div>
- )
- 
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,25 @@
 import '../styles/Cart.css'
+import { useState } from 'react'
 function Cart()
 {
-    const monstera = 8;
-    const lierre = 10;
-    const bouquet = 15;
-     return (<div className="img-cart">
-        <h2>Panier</h2>
-        <ul>
-            <li>monstera : {monstera}$</li>
-            <li>lierre : {lierre}$</li>
-            <li>bouquet de fleurs : {bouquet}$</li>
-            </ul>
-            <p>total : {monstera+lierre+bouquet}$</p>
+    const monsteraPrice = 8
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+ 
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
+            <h2>Panier</h2>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
             </div>
-            );
+            <h3>Total : {monsteraPrice * cart}€</h3>
+        </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+    )
 }
 export default Cart;
